c99a92d3e98527bbab99fb1eb05d23262c28aca2
dylangrandmont <dylan.grandmont@gmail.com>
2021-10-04 16:56:19 -0600

    extract wheel navigation component

---
+++ navigation-wheel.js
@@ -0,0 +1,17 @@
+class NavigationWheel extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.innerHTML = `
+        <a href="/" style="
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: min-content;
+    ">
+            <img src="/ferris-svgrepo-com.svg" width="50">
+            ←
+        </a>        `;
+    }
+}
+window.customElements.define('navigation-wheel', NavigationWheel)
