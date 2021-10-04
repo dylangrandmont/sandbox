12e074f56100160aba7e0d0f2dfeafec769689c5
dylangrandmont <dylan.grandmont@gmail.com>
2021-10-04 16:50:53 -0600

    add topic container custom component and tag sandboxes with topics

---
+++ topic-container.js
@@ -0,0 +1,36 @@
+class TopicContainer extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.innerHTML = `
+        <style>
+        h6 {
+            display: flex;
+            align-items: center;
+            gap: 4px;
+        }
+        .topic {
+            background-color: var(--palette-main-5);
+            color: var(--palette-main-0);
+            border-radius: 8px;
+            padding: 2px 4px;
+}
+    </style>
+        `;
+    }
+    
+    connectedCallback() {
+        const h6 = document.createElement('h6');
+        const topics = this.getAttribute('topics')
+        h6.innerText = 'Topics: ';
+        console.log(topics)
+        topics.split(',').forEach(topic => {
+            const element = document.createElement('div')
+            element.classList.add('topic')
+            element.innerText = topic;
+            h6.appendChild(element)
+        })
+        this.shadowRoot.appendChild(h6);
+    }
+}
+window.customElements.define('topic-container', TopicContainer)
