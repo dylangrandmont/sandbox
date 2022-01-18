86e0e0522542b358570f77fbdbf5e1340f9b332f
Dylan Grandmont <dylan.grandmont@gmail.com>
2022-01-18 10:46:19 -0700

    bump up total on web worker sandbox to illustrate effect more

--- js/webworker/calc.js
+++ js/webworker/calc.js
@@ -1,8 +1,8 @@
-onmessage = function(event) {
-    let total = 0;
-    for (let i = 0; i < 1000000000; i++) {
-        total = total + 1;
-    }
-    postMessage(total)
-    return
-}
+onmessage = function (event) {
+  let total = 0;
+  for (let i = 0; i < 10000000000; i++) {
+    total = total + 1;
+  }
+  postMessage(total);
+  return;
+};
--- js/webworker/main.js
+++ js/webworker/main.js
@@ -1,48 +1,48 @@
 function logToResultDiv(text) {
-    const element = document.getElementById('result');
-    const existingText = element.innerHTML;
-    element.innerHTML = `${existingText} <p>${text}</p>`;
+  const element = document.getElementById("result");
+  const existingText = element.innerHTML;
+  element.innerHTML = `${existingText} <p>${text}</p>`;
 }
 
 function clearResults() {
-    const element = document.getElementById('result');
-    element.innerHTML = '';
+  const element = document.getElementById("result");
+  element.innerHTML = "";
 }
 
 let startTime;
 
 function start() {
-    startTime = new Date();
-};
-  
-function end() {
-    const endTime = new Date();
-    const timeDiff = endTime - startTime;
-    const seconds = Math.round(timeDiff);
+  startTime = new Date();
+}
 
-    logToResultDiv(`Event Loop blocked for ${seconds} ms`);
+function end() {
+  const endTime = new Date();
+  const timeDiff = endTime - startTime;
+  const seconds = Math.round(timeDiff);
+
+  logToResultDiv(`Event Loop blocked for ${seconds} ms`);
 }
 
 function calculateWithWebWorker() {
-    start();
-    const worker = new Worker('calc.js');
-    worker.onmessage = function (event) {
-        logToResultDiv(`Calculation Result: ${event.data}`)
-    }
-    worker.onerror = function(error) {
-        logToResultDiv(`Worker error:  ${error.message}`);
-        throw error;
-    };
-    worker.postMessage(1000000000);
-    end();
+  start();
+  const worker = new Worker("calc.js");
+  worker.onmessage = function (event) {
+    logToResultDiv(`Calculation Result: ${event.data}`);
+  };
+  worker.onerror = function (error) {
+    logToResultDiv(`Worker error:  ${error.message}`);
+    throw error;
+  };
+  worker.postMessage(10000000000);
+  end();
 }
 
 function calculateOnMainThread() {
-    start();
-    let total = 0;
-    for (let i = 0; i < 1000000000; i++) {
-        total = total + 1;
-    }
-    logToResultDiv(`Calculation Result: ${total}`);
-    end();
+  start();
+  let total = 0;
+  for (let i = 0; i < 10000000000; i++) {
+    total = total + 1;
+  }
+  logToResultDiv(`Calculation Result: ${total}`);
+  end();
 }
